9e9e6785a6964fb19be8d5f2a66370156da4f7f2
Allan Silva Barroso de Oliveira <38400010+allansbo@users.noreply.github.com>
2022-07-18 22:07:46 -0300

    Consome API para buscar os top 250 filmes do IMDB

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception{
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // extrair somentes os dados que interessam (titulo, poster, avaliação)
+
+        // exibir e manipular os dados
     }
 }
