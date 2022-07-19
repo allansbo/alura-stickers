dcf815ce598c9950f1e34a6789284c6130f07121
Allan Silva Barroso de Oliveira <38400010+allansbo@users.noreply.github.com>
2022-07-18 22:47:27 -0300

    Parser das informações extraidas do JSON e exibição dos dados em terminal

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception{
@@ -11,14 +13,21 @@ public class App {
         String url = "https://alura-filmes.herokuapp.com/conteudos";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
-
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         // extrair somentes os dados que interessam (titulo, poster, avaliação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        System.out.println(listaDeFilmes.size());
 
         // exibir e manipular os dados
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
